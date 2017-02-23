dfd8a808868a5c15be7fc9ca02061faec9c4105d
DerekYu <derek-nis@hotmail.com>
2017-02-23 11:59:52 -0500

    creating register file array 32x32 and process to write to it

--- AssemblerController/testbench.tcl
+++ AssemblerController/testbench.tcl
@@ -24,9 +24,24 @@ component pipeline is
   );
 end component;
 
+signal clock_period : time := 1 ns;
+
 signal PROGRAM_COUNTER : integer;
+signal PROGRAM_FINISHED : std_logic;
+
+-- use this to store our register values until PROGRAM_FINISHED = '1' at which point we "flush" the register_file to register_file.txt
+type register_type is array (31 downto 0) of std_logic_vector(31 downto 0);
+signal register_file : register_type := (others => (others => '0'));
 
 begin
+
+  clock_process : process
+  begin
+    clock <= '0';
+    wait for clock_period / 2;
+    clock <= '1';
+    wait for clock_period / 2;
+  end process;
 
   read_program : process (ready)
     file program : TEXT open READ_MODE is "program.txt";
@@ -43,12 +58,12 @@ begin
     wait;
   end process read_program;
 
-  write_register_file : process(done)
-    file rester : TEXT open WRITE_MODE is "register_file.txt";
+  write_register_file : process (PROGRAM_FINISHED)
+    file register : TEXT open WRITE_MODE is "register_file.txt";
     variable write_line : LINE;
     variable line_input : LINE;
   begin
-    if done = '1' then
+    if PROGRAM_FINISHED = '1' then
       -- plenty of conditions here
     end if;
   end process write_register_file;
