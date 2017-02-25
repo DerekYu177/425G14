55b899279ebbf48de305026770a2016fa081a1a9
DerekYu <derek-nis@hotmail.com>
2017-02-25 18:25:58 -0500

    modifying program read to convert text to std_logic_vector

--- PipelineController/testbench.tcl
+++ PipelineController/testbench.tcl
@@ -32,10 +32,14 @@ end component;
 
 -- constants
 constant clock_period : time := 1 ns;
-constant memory_size : integer := 8191;
-constant register_size : integer := 31;
+constant data_size : integer := 32;
+constant memory_size : integer := 8192;
+constant register_size : integer := 32;
 constant memory_line_counter : integer := 0;
 constant register_line_counter : integer := 0;
+
+-- binary logic vectors for reading text to std_logic_vector
+signal program_binary_data : std_logic_vector(31 downto 0) := (others => '0');
 
 -- input port map signals
 signal clock : std_logic;
@@ -50,7 +54,7 @@ signal program_execution_finished : std_logic;
 signal memory_out_finished : std_logic;
 signal register_out_finished : std_logic;
 signal memory_out : std_logic_vector(31 downto 0);
-signal register_out : std_logic_vector(31 downto 0)
+signal register_out : std_logic_vector(31 downto 0);
 
 begin
 
@@ -79,18 +83,30 @@ begin
 
   read_program : process (reset)
     file program : TEXT is in "program.txt";
-    variable inline : LINE;
-    variable line_output : REAL;
+    variable line_number : LINE;
+    variable line_content : REAL;
+    variable i : integer := '0';
+    variable char : character := '0';
   begin
     if reset = '1' then
-      wait until clock'event and clock = '1';
-      if (not endfile(program)) then
-        readline(program, inline);
-        read(inline, line_output);
+      while (not endfile(program)) loop
+        wait until clock'event and clock = '1';
+        readline(program, line_number);
+        read(line_number, line_content);
 
-        -- there is probably going to be a real -> std_logic_vector conflict here
-        program_in <= line_output;
-      end if;
+        -- for each text character in the line_content
+        -- convert into std_logic vector to be sent
+        for i in 1 to data_size loop
+          char := line_content(i);
+          if (char = '0') then
+            program_binary_data(data_size - i) <= '0';
+          else
+            program_binary_data(data_size - i) <= '1';
+          end if;
+        end loop;
+
+        program_in <= program_binary_data;
+      end loop;
     end if;
     wait;
   end process read_program;
