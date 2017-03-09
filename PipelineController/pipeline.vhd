1ed48253389b87fd09d39f10af8d40f68ca53d74
DerekYu <derek-nis@hotmail.com>
2017-03-09 01:25:02 -0500

    [Derek] adding correct components without mux to the pipeline.vhd file

--- PipelineController/pipeline.vhd
+++ PipelineController/pipeline.vhd
@@ -25,7 +25,7 @@ architecture arch of pipeline is
     ready, initializing, finishing,
     instruction_fetch, instruction_decode, execute, memory, writeback
   );
-  
+
   signal present_state, next_state : state_type;
   -- signal if_id, id_ex, ex_m, m_wb : std_logic_vector(31 downto 0);
 
@@ -37,8 +37,70 @@ architecture arch of pipeline is
   signal memory_line_counter : integer := 0;
   signal register_line_counter : integer := 0;
   signal read_write_finished : boolean := false;
-  
-  
+
+  -- DECLARING COMPONENTS --
+
+  component instruction_memory
+    generic(
+  		ram_size : integer := 1024;
+  		mem_delay : time := 10 ns;
+  		clock_period : time := 1 ns
+  	);
+  	port(
+  		clock : in std_logic;
+  		writedata : in std_logic_vector(31 downto 0);
+
+  		address : in integer range 0 to ram_size-1;
+  		memwrite : in std_logic;
+  		memread : in std_logic;
+  		readdata : out std_logic_vector(31 downto 0);
+  		waitrequest : out std_logic
+  	);
+  end component;
+
+  component data_memory
+    generic(
+      ram_size : integer := 8192;
+      mem_delay : time := 10 ns;
+      clock_period : time := 1 ns
+    );
+  	port(
+  		clock : in std_logic;
+  		writedata : in std_logic_vector(31 downto 0);
+
+  		address : in integer range 0 to ram_size-1;
+  		memwrite : in std_logic;
+  		memread : in std_logic;
+  		readdata : out std_logic_vector(31 downto 0);
+  		waitrequest : out std_logic
+  	);
+  end component;
+
+  component registers
+    port(
+      clock : in std_logic;
+  		writedata : in std_logic_vector(31 downto 0);
+  		readreg1 : in integer range 0 to 31;
+  		readreg2 : in integer range 0 to 31;
+  		writereg : in integer range 0 to 31;
+
+  		regwrite : in std_logic;
+  		readdata1 : out std_logic_vector(31 downto 0);
+  		readdata2 : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
+  component ALU is
+    port(
+      clock, reset : in std_logic;
+      ALU_instruction : in std_logic_vector(31 downto 0);
+      ALU_operand1 : in std_logic_vector(31 downto 0);
+      ALU_operand2 : in std_logic_vector(31 downto 0);
+      ALU_NPC : in std_logic_vector(31 downto 0);
+      ALU_output : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
   begin
 
     async_operation : process(clock, reset)
