0125ac80d12b83c2b433ea72a00270e4f06b40cc
DerekYu <derek-nis@hotmail.com>
2017-03-26 15:51:45 -0400

    updating the tcl

--- PipelineController/testbench.tcl
+++ PipelineController/testbench.tcl
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
 
 library std;
 use std.textio.all;
@@ -17,9 +18,7 @@ component PIPELINE
 
   -- inputs --
   program_in : in std_logic_vector(31 downto 0);
-  memory_in : in std_logic_vector(31 downto 0);
   program_in_finished : in std_logic;
-  memory_in_finished : in std_logic;
 
   -- outputs --
   program_execution_finished : out std_logic;
@@ -35,42 +34,38 @@ constant clock_period : time := 1 ns;
 constant data_size : integer := 32;
 constant memory_size : integer := 8192;
 constant register_size : integer := 32;
-constant memory_line_counter : integer := 0;
-constant register_line_counter : integer := 0;
+constant byte_size : integer := 8;
 
--- binary logic vectors for reading text to std_logic_vector
-signal program_binary_data : std_logic_vector(31 downto 0) := (others => '0');
+-- read/write
+signal write_finished : boolean := false;
+file register_file, memory : text;
 
 -- input port map signals
-signal clock : std_logic;
-signal reset : std_logic;
+signal clock : std_logic := '0';
+signal reset : std_logic := '0';
 signal program_in : std_logic_vector(31 downto 0);
-signal memory_in : std_logic_vector(31 downto 0);
-signal program_in_finished : std_logic;
-signal memory_in_finished : std_logic;
+signal program_in_finished : std_logic := '0';
 
 -- output port map signals
-signal program_execution_finished : std_logic;
-signal memory_out_finished : std_logic;
-signal register_out_finished : std_logic;
+signal program_execution_finished : std_logic := '0';
+signal memory_out_finished : std_logic := '0';
+signal register_out_finished : std_logic := '0';
 signal memory_out : std_logic_vector(31 downto 0);
 signal register_out : std_logic_vector(31 downto 0);
 
 begin
 
   P : PIPELINE port map (
-    clock => clock,
-    reset => reset,
-    program_in => program_in,
-    memory_in => memory_in,
-    program_in_finished => program_in_finished,
-    memory_in_finished => memory_in_finished,
+    clock,
+    reset,
+    program_in,
+    program_in_finished,
 
-    program_execution_finished => program_execution_finished,
-    memory_out_finished => memory_out_finished,
-    register_out_finished => register_out_finished,
-    memory_out => memory_out,
-    register_out => register_out
+    program_execution_finished,
+    memory_out_finished,
+    register_out_finished,
+    memory_out,
+    register_out
   );
 
   clock_process : process
@@ -81,62 +76,90 @@ begin
     wait for clock_period / 2;
   end process;
 
-  read_program : process (reset)
-    file program : TEXT is in "program.txt";
-    variable line_number : LINE;
-    variable line_content : REAL;
-    variable i : integer := '0';
-    variable char : character := '0';
+  read_program : process
+    file program : text; -- open read_mode is "program.txt"
+    variable line_number : line;
+    variable line_content : std_logic_vector(31 downto 0);
   begin
-    if reset = '1' then
-      while (not endfile(program)) loop
-        wait until clock'event and clock = '1';
-        readline(program, line_number);
-        read(line_number, line_content);
+    reset <= '1';
 
-        -- for each text character in the line_content
-        -- convert into std_logic vector to be sent
-        for i in 1 to data_size loop
-          char := line_content(i);
-          if (char = '0') then
-            program_binary_data(data_size - i) <= '0';
-          else
-            program_binary_data(data_size - i) <= '1';
-          end if;
-        end loop;
+    report "opening program";
+    file_open(program, "program.txt", READ_MODE);
 
-        program_in <= program_binary_data;
-      end loop;
-    end if;
+    report "endfile? : " & boolean'image(endfile(program));
+    while (not endfile(program)) loop
+      wait until clock'event and clock = '1';
+
+      report "reading line from program";
+      readline(program, line_number);
+      read(line_number, line_content);
+
+      report "writing program line to pipeline";
+      program_in <= line_content;
+
+      wait for clock_period;
+
+    end loop;
+
+    report "end of file";
+    file_close(program);
+    reset <= '0';
+    program_in_finished <= '1';
     wait;
   end process read_program;
 
-  write_register_file : process (program_execution_finished)
-    file register_file : TEXT is out "register_file.txt";
-    variable outline : LINE;
+  write_register_memory_files : process(program_execution_finished)
+      -- Based on https://www.nandland.com/vhdl/examples/example-file-io.html
+      variable l_register, l_memory : line;
+      variable v_register_out, v_memory_out : std_logic_vector(31 downto 0);
+
   begin
     if program_execution_finished = '1' then
-      wait until clock'event and clock = '1';
-      write(outline, register_out); -- do we require field(width) and digits(natural)?
-      writeline(register_file, outline);
-      -- plenty of conditions here
-      -- increment counter
-      -- if counter = counter_max_value, stop
-    end if;
-  end process write_register_file;
 
-  write_memory_file : process (program_execution_finished)
-    file memory : TEXT is out "memory.txt";
-    variable outline : LINE;
-  begin
-      if program_execution_finished = '1' then
-      wait until clock'event and clock = '1';
-      write(outline, memory_out); -- do we require field(width) and digits(natural)?
-      writeline(memory, outline);
-      -- plenty of conditions here
-      -- increment counter
-      -- if counter = counter_max_value, stop
+      file_open(register_file, "register_file.txt", write_mode);
+      file_open(memory, "memory.txt", write_mode);
+
+      if clock'event and clock = '1' then
+        while (not write_finished) loop
+
+          if (register_out_finished = '0') then
+            v_register_out <= register_out;
+
+            write(l_register, v_register_out);
+            writeline(register_file, l_register);
+          else
+            -- register write finished
+            file_close(register_file);
+          end if;
+
+          if (memory_out_finished = '0') then
+            v_memory_out <= memory_out;
+
+            write(l_memory, v_memory_out);
+            writeline(memory, l_memory);
+          else
+            -- memory write finished
+            file_close(memory);
+          end if;
+
+          if (register_out_finished = '1' and memory_out_finished = '1') then
+            write_finished <= true;
+          end if;
+
+        end loop;
+      end if;
     end if;
-  end process write_memory_file;
+  end process write_register_memory_files;
+
+  test_process : process
+  begin
+    report "simulation starting";
+    -- first try reading from a program with a single line of text
+    -- wait the appropriate amount of clock cycles for program to be sent
+    wait until program_in_finished = '1';
+    -- wait until the pipeline is finished with it's calculation
+    wait until program_execution_finished = '1';
+    -- we'll have to manually check the file?
+    end process;
 
 end architecture behavior;
