339d61ee5fd1bd36df52c715b2af2bad203a7ade
Markus Suorsa <markus.suorsa@gmail.com>
2017-03-30 17:11:20 -0400

    whatever derek

--- PipelineController/Test_Programs/test4/test4.asm
+++ PipelineController/Test_Programs/test4/test4.asm
@@ -1,4 +1,4 @@
 addi $1, $0, 1 #place 1 into regsiter 1
-addi $0, $0, 0
-addi $0, $0, 0 #stall to avoid data hazard
+addi $2, $0, 2
+addi $3, $0, 3 #stall to avoid data hazard
 sw $1, 0($0)  #store contents of reg 1 into memory address 0
