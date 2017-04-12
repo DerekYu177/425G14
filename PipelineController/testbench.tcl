c31d03b08f0e25f4cc807a76c76b9274d3cdd4d6
DerekYu <derek-nis@hotmail.com>
2017-04-11 21:43:48 -0400

    [Final] adding the .vhd -> .tcl for easier scripting

--- PipelineController/testbench.tcl
+++ PipelineController/testbench.tcl
@@ -1,10 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_textio.all;
-
-library std;
 use std.textio.all;
+use ieee.std_logic_textio.all;
 
 entity pipeline_tb is
 end pipeline_tb;
@@ -36,9 +34,28 @@ constant memory_size : integer := 8192;
 constant register_size : integer := 32;
 constant byte_size : integer := 8;
 
--- read/write
-signal write_finished : boolean := false;
+-- Input control signals
+file program : text;
+signal input_initalize_flag : std_logic := '0';
+signal r_line_content_1 : std_logic_vector(31 downto 0);
+signal r_line_content_2 : std_logic_vector(31 downto 0);
+
+-- input FSM --
+type input_state_type is (
+  input_initialize,
+  read_1, read_2,
+  end_read
+);
+
+signal present_state, next_state : input_state_type;
+
+-- Output control signals
 file register_file, memory : text;
+signal register_open_file : std_logic := '0';
+signal memory_open_file : std_logic := '0';
+signal output_initialize_flag : std_logic := '0';
+constant c_width : natural := 32;
+constant b_width : natural := 8;
 
 -- input port map signals
 signal clock : std_logic := '0';
@@ -52,6 +69,7 @@ signal memory_out_finished : std_logic := '0';
 signal register_out_finished : std_logic := '0';
 signal memory_out : std_logic_vector(31 downto 0);
 signal register_out : std_logic_vector(31 downto 0);
+signal program_terminated : std_logic := '0';
 
 begin
 
@@ -76,90 +94,81 @@ begin
     wait for clock_period / 2;
   end process;
 
-  read_program : process
-    file program : text; -- open read_mode is "program.txt"
-    variable line_number : line;
-    variable line_content : std_logic_vector(31 downto 0);
+  io : process
+    variable v_program_line_1: line;
+    variable v_line_content_1: std_logic_vector(31 downto 0);
+    variable v_register_line, v_memory_line : line;
   begin
+    file_open(program, "program.txt", read_mode);
+
     reset <= '1';
+    wait for clock_period/2;
+    reset <= '0';
 
-    report "opening program";
-    file_open(program, "program.txt", READ_MODE);
+    wait for clock_period;
 
-    report "endfile? : " & boolean'image(endfile(program));
-    while (not endfile(program)) loop
-      wait until clock'event and clock = '1';
-
-      report "reading line from program";
-      readline(program, line_number);
-      read(line_number, line_content);
-
-      report "writing program line to pipeline";
-      program_in <= line_content;
-
+    while not endfile(program) loop
+      readline(program, v_program_line_1);
+      read(v_program_line_1, v_line_content_1);
+      r_line_content_1 <= v_line_content_1;
+      program_in <= v_line_content_1;
       wait for clock_period;
-
     end loop;
 
-    report "end of file";
     file_close(program);
-    reset <= '0';
-    program_in_finished <= '1';
-    wait;
-  end process read_program;
 
-  write_register_memory_files : process(program_execution_finished)
-      -- Based on https://www.nandland.com/vhdl/examples/example-file-io.html
-      variable l_register, l_memory : line;
-      variable v_register_out, v_memory_out : std_logic_vector(31 downto 0);
+    program_in_finished <= '1';
+
+    wait until program_execution_finished = '1';
+    wait until program_terminated = '1';
+
+  end process;
+
+  write_register_files : process(program_execution_finished, clock)
+    -- Based on https://www.nandland.com/vhdl/examples/example-file-io.html
+    variable v_register_line, v_memory_line : line;
 
   begin
+
     if program_execution_finished = '1' then
 
-      file_open(register_file, "register_file.txt", write_mode);
-      file_open(memory, "memory.txt", write_mode);
+      if register_open_file = '0' and output_initialize_flag = '0' then
+        register_open_file <= '1';
+        memory_open_file <= '1';
+        output_initialize_flag <= '1';
+        file_open(register_file, "register_file.txt", write_mode);
+        file_open(memory, "memory.txt", write_mode);
+      end if;
 
       if clock'event and clock = '1' then
-        while (not write_finished) loop
 
-          if (register_out_finished = '0') then
-            v_register_out <= register_out;
+        if register_out_finished = '0' then
+          write(v_register_line, register_out, right, c_width);
+          writeline(register_file, v_register_line);
+        end if;
 
-            write(l_register, v_register_out);
-            writeline(register_file, l_register);
-          else
-            -- register write finished
-            file_close(register_file);
-          end if;
+        if memory_out_finished = '0' then
+          write (v_memory_line, memory_out, right, b_width);
+          writeline(memory, v_memory_line);
+        end if;
 
-          if (memory_out_finished = '0') then
-            v_memory_out <= memory_out;
-
-            write(l_memory, v_memory_out);
-            writeline(memory, l_memory);
-          else
-            -- memory write finished
-            file_close(memory);
-          end if;
-
-          if (register_out_finished = '1' and memory_out_finished = '1') then
-            write_finished <= true;
-          end if;
-
-        end loop;
       end if;
-    end if;
-  end process write_register_memory_files;
 
-  test_process : process
-  begin
-    report "simulation starting";
-    -- first try reading from a program with a single line of text
-    -- wait the appropriate amount of clock cycles for program to be sent
-    wait until program_in_finished = '1';
-    -- wait until the pipeline is finished with it's calculation
-    wait until program_execution_finished = '1';
-    -- we'll have to manually check the file?
-    end process;
+      if register_open_file = '1' and register_out_finished = '1' then
+        register_open_file <= '0';
+        file_close(register_file);
+      end if;
+
+      if memory_open_file = '1' and memory_out_finished = '1' then
+        memory_open_file <= '0';
+        file_close(memory);
+      end if;
+
+      if register_out_finished = '1' and memory_out_finished = '1' then
+        program_terminated <= '1';
+      end if;
+
+    end if;
+  end process write_register_files;
 
 end architecture behavior;
